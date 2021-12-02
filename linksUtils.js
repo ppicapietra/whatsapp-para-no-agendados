327edde032715946f88acc3f59a5c5216ac32a26
thenablyn <ppicapietra@gmail.com>
2021-12-02 00:30:44 -0300

    1. Change. Encapsulate links methods in Object "Utils".

--- linksUtils.js
+++ linksUtils.js
@@ -1,24 +1,27 @@
-function getLink(cellphoneNumber, message = null) {
-    try {
-        return encodeURI(`https://wa.me/+549${cellphoneNumber}${((message) ? (`&text=${message}`) : '')}`);
+const Utils = {
+
+    getLink: (cellphoneNumber, message = null) => {
+        try {
+            return encodeURI(`https://wa.me/+549${cellphoneNumber}${((message) ? (`&text=${message}`) : '')}`);
+        }
+        catch (e) { }
+    },
+ validateCellphoneNumber: (cellphoneNumber) => {
+        try {
+            return !((cellphoneNumber == "") || (cellphoneNumber.length < 9));
+        }
+        catch (e) { }
+    },
+formatCellphoneNumber: (cellphoneNumber) => {
+        try {
+            return cellphoneNumber.replace(/[^0-9]/g, "").replace(/^(54)?9?/g, "");
+        }
+        catch (e) { }
+    },
+openChat: (url) => {
+        try {
+            window.location = url;
+        }
+        catch (e) { }
     }
-    catch (e) { }
-}
-function validateCellphoneNumber(cellphoneNumber) {
-    try {
-        return !((cellphoneNumber == "") || (cellphoneNumber.length < 9));
-    }
-    catch (e) { }
-}
-function formatCellphoneNumber(cellphoneNumber) {
-    try {
-        return cellphoneNumber.replace(/[^0-9]/g, "").replace(/^(54)?9?/g, "");
-    }
-    catch (e) { }
-}
-function openChat(url) {
-    try {
-        window.location = url;
-    }
-    catch (e) { }
 }
