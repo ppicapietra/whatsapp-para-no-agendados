9752cc9158073d230e48f5de1e4b830bfc10d47f
thenablyn <ppicapietra@gmail.com>
2021-12-03 16:44:55 -0300

    all

--- sw.js
+++ sw.js
@@ -1,34 +1,46 @@
 const versionNumber = "1.2.1";
-let cacheName = 'wncs-pwa';
+let cacheName = 'wanoa-cache-v1';
 let filesToCache = [
   './images/whatsapp-16.png',
   './images/whatsapp-128.png',
   './images/whatsapp-256.png',
   './images/whatsapp-512.png',
   './whatsapp-msg.html',
-  './linksUtils.js'  
+  './linksUtils.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener( 'install', ( e ) => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    navigator.registerProtocolHandler('web+wanoa', 'https://grupopietra.com/tools/wsp-ncs/whatsapp-msg.html?n=%s');
-    caches.open( cacheName ).then( ( cache ) => {
-      return cache.addAll( filesToCache );
-    })
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    }).then(() => navigator.registerProtocolHandler('web+wanoa', 'n=%s'))
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener( 'fetch', ( e ) => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match( e.request, { ignoreSearch: true }).then( ( response ) => {
-      return response || fetch( e.request );
+    caches.match(e.request, { ignoreSearch: true }).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
 
+/* Delete old caché when this one start working after an app update */
+self.addEventListener('activate', (e) => {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+
+});
 /* App launched */
-self.addEventListener( 'launch', async ( e ) => {
-  
+self.addEventListener('launch', async (e) => {
+
 });
