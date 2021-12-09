3db8b006e78a249afea876e780ddc553c03faea7
thenablyn <ppicapietra@gmail.com>
2021-12-08 21:03:28 -0300

    change url handler definition

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const versionNumber = "1.2.1";
+const versionNumber = "1.3.0";
 let cacheName = 'wanoa-cache-v1';
 let filesToCache = [
   './images/whatsapp-16.png',
@@ -14,7 +14,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(filesToCache);
-    }).then(() => navigator.registerProtocolHandler('web+wanoa', 'n=%s'))
+    }).then(() => navigator.registerProtocolHandler('web+wanoa', 'https://grupopietra.com/tools/wsp-ncs/whatsapp-msg.html?q=%s',"WaNoA"))
   );
 });
 
